fcf1598b687737a0f1907bb5cf2e8d2064f703cf
behruz000 <tu78qvlle@code.edu.az>
2022-11-16 02:17:44 +0400

    feat:added

---
+++ loops/loops/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          var count=0;
+            int  [] test = {1,2,3,4,5,6,7,8,9};
+            
+
+            for (int i = 0; i < test.Length ; i++)
+            {
+                if (test[i] % 2 == 1) 
+                {
+                  
+                    count++;    
+
+
+                
+                }
+            }
+            Console.WriteLine("tek ededlerin sayi :"+count);   
+           
+
+
+
+
+
+
+        } 
+
+    }
+}
